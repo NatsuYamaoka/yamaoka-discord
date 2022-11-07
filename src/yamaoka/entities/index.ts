d3e5ba4195dfa379461b9f230d990fc4e6b95817
sekkure <secreori@gmail.com>
2022-11-07 05:54:03 +0200

    ref: Removed yamaoka folder, moved all folders on one level up

--- src/yamaoka/entities/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./user.entity";
-export * from "./quiz.entity";
-export * from "./wallet.entity";
-export * from "./quiz-question.entity";
-export * from "./completed-quiz.entity";
--- src/yamaoka/typings/enums.ts
+++
@@ -1,5 +0,0 @@
-export enum ScrollButtons {
-  TO_LEFT = "to-left-button",
-  TO_STOP = "to-stop-button",
-  TO_RIGHT = "to-right-button",
-}
