3b1b2f54f4e19eb37454411a1ec43a4bd213ad58
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-03-04 11:23:25 +0200

    wip: Add set-profile command

--- src/modules/general/general.module.ts
+++ src/modules/general/general.module.ts
@@ -1,8 +1,9 @@
 import { Base } from "@abstracts/client/client.abstract";
 import { Module } from "@decorators/module.decorator";
 import { ProfileCommand } from "@modules/general/slash-commands/profile.command";
+import { SetProfileCommand } from "./slash-commands/set-profile.command";
 
 @Module({
-  commands: [ProfileCommand],
+  commands: [ProfileCommand, SetProfileCommand],
 })
 export class GeneralModule extends Base {}
