012f60d797d41a4fdd2dd131f5bee270b5b500f7
ItsRichardScull <totallynotrichardscull@gmail.com>
2024-03-04 11:29:50 +0200

    Revert "wip: Add set-profile command"
    Moved to a new branch to be worked on later
    This reverts commit 3b1b2f54f4e19eb37454411a1ec43a4bd213ad58.

--- src/modules/general/general.module.ts
+++ src/modules/general/general.module.ts
@@ -1,9 +1,8 @@
 import { Base } from "@abstracts/client/client.abstract";
 import { Module } from "@decorators/module.decorator";
 import { ProfileCommand } from "@modules/general/slash-commands/profile.command";
-import { SetProfileCommand } from "./slash-commands/set-profile.command";
 
 @Module({
-  commands: [ProfileCommand, SetProfileCommand],
+  commands: [ProfileCommand],
 })
 export class GeneralModule extends Base {}
